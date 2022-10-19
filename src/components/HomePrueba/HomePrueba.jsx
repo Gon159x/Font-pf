f5c4b17d19740bc8ab0bb9b401db1600007472aa
Gon159x <gonzaxx5@gmail.com>
2022-10-19 15:50:45 -0300

    lpm arreglate

--- src/components/HomePrueba/HomePrueba.jsx
+++ src/components/HomePrueba/HomePrueba.jsx
@@ -84,7 +84,8 @@ export default function HomePrueba(){
                                     name = {worker.User.name}
                                     lastName = {worker.User.lastName}
                                     job = {worker.Jobs[0].name}
-                                    country = {worker.User.Country.name !== null? worker.User.Country.name: "Indefinido"}
+
+                                    country = {"undefined"}//{worker.User.Country.name !== null? worker.User.Country.name: "Indefinido"}
                                     city = {worker.User.city}
                                     premium = {worker.premium}
                                     img = {worker.User.img}
